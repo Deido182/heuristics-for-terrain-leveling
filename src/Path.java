1468418326cd5af7b642201798248bc3160d8282
DavideFranchi97 <davide9706@gmail.com>
2019-03-01 11:16:30 +0100

    Moved some methods from Truck to Path

--- src/Path.java
+++ src/Path.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 public class Path {
 	private class Stopover {
@@ -8,5 +9,59 @@ public class Path {
 			this.coordinates = coordinates;
 			this.quantityToBringIn = quantityToBringIn;
 		}
+	}
+	
+	ArrayList <Stopover> stopovers;
+	
+	public Path() {
+		stopovers = new ArrayList <> ();
+	}
+	
+	public int length() {
+		return stopovers.size();
+	}
+	
+	public Stopover remove(int i) {
+		return stopovers.remove(i);
+	}
+	
+	private void addStopover(int i, Stopover s) {
+		stopovers.add(i, s);
+	}
+	
+	private void addStopover(Stopover s) {
+		addStopover(length(), s);
+	}
+	
+	public void addStopover(int i, Coordinates c, double q) {
+		addStopover(i, new Stopover(c, q));
+	}
+	
+	public void addStopover(Coordinates c, double q) {
+		addStopover(length(), c, q);
+	}
+	
+	public void addStopover(int i, Coordinates c) {
+		Stopover removed = remove(i + 1);
+		addStopover(i + 1, c, removed.quantityToBringIn);
+		addStopover(i + 2, removed);
+	}
+	
+	public void addTwoStopovers(int i, Coordinates c1, Coordinates c2) {
+		addStopover(i, c1);
+		addStopover(i + 1, c2);
+	}
+	
+	public void append(Path p) {
+		for(Stopover s : p.stopovers)
+			addStopover(s);
+	}
+	
+	public Movement getMovement(int i) {
+		return new Movement(stopovers.get(i).coordinates, stopovers.get(i + 1).coordinates, stopovers.get(i + 1).quantityToBringIn);
+	}
+	
+	public Movement getLastMovement() {
+		return getMovement(length() - 2);
 	}
 }
--- src/Truck.java
+++ src/Truck.java
@@ -4,54 +4,24 @@ import java.util.ArrayList;
 public class Truck {
 	double capacity;
 	double minimumMove;
-	Coordinates currentPosition;
-	ArrayList <Movement> path;
+	Path path;
 	
-	public Truck(double capacity, double minimumMove, Coordinates currentPosition) {
+	public Truck(double capacity, double minimumMove, Coordinates from) {
 		this.capacity = capacity;
 		this.minimumMove = minimumMove;
-		this.currentPosition = currentPosition;
-		path = new ArrayList <> ();
+		this.path = new Path();
+		path.addStopover(from, 0.0);
 	}
 	
 	public void move(Coordinates to, double quantity) throws IOException {
-		if(quantity < 0 || quantity > capacity)
-			throw new IOException("The required quantity is not valid");
-		path.add(new Movement(currentPosition, to, quantity));
-		currentPosition = to;
+		path.addStopover(to, quantity);
 	}
 	
 	public void move(Coordinates to) throws IOException {
 		move(to, 0.0);
 	}
 	
-	public void move(ArrayList <Movement> movements) {
-		if(currentPosition != null)
-			path.add(new Movement(currentPosition, movements.get(0).from));
-		path.addAll(movements);
-		currentPosition = getLastMovement().to;
-	}
-	
-	public Movement getMovement(int i) {
-		return path.get(i);
-	}
-	
-	public void removeMovement(int i) {
-		path.remove(i);
-	}
-	
-	public Movement getLastMovement() {
-		return getMovement(path.size() - 1);
-	}
-	
-	public void addStopoverAfter(Coordinates stopover, int i) {
-		Movement removed = path.remove(i + 1);
-		path.add(i + 1, new Movement(getMovement(i).to, stopover, removed.quantity));
-		path.add(i + 2, new Movement(stopover, removed.to, removed.quantity));
-	}
-	
-	public void addStopoversAfter(ArrayList <Coordinates> stopovers, int i) {
-		addStopoverAfter(stopovers.get(0), i);
-		addStopoverAfter(stopovers.get(1), i + 1);
+	public void move(Path p) {
+		path.append(p);
 	}
 }
